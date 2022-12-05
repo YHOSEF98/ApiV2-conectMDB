8fa37a8bd641011470613eec764e8039a746b02b
YHOSEF MAYORGA - C <mayorgalive@hotmail.com>
2022-12-05 17:40:59 -0500

    añadir api 2

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response, JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field, EmailStr
-from bson.objectid import ObjectId
+from bson import ObjectId
 from typing import Optional, List
 import motor.motor_asyncio
 import pymongo
 
 app = FastAPI()
-#client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["mongodb+srv://yhosefmayorga:1065245907Mc-@cluster0.uidz2ja.mongodb.net/?retryWrites=true&w=majority"])
-client = pymongo.MongoClient("mongodb+srv://yhosefmayorga:1065245907Mc-@cluster0.uidz2ja.mongodb.net/?retryWrites=true&w=majority")
-mydb = client["MinTicVehiculos"]
-db = mydb["Vehiculos"]
+
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.VehiculosYhosef
 
 class PyObjectId(ObjectId):
     @classmethod
@@ -21,7 +20,7 @@ class PyObjectId(ObjectId):
 
     @classmethod
     def validate(cls, v):
-        if not isinstance(v, ObjectId):
+        if not ObjectId.is_valid(v):
             raise ValueError("Invalid objectid")
         return ObjectId(v)
 
@@ -29,14 +28,13 @@ class PyObjectId(ObjectId):
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
 
-        
-class VehiculosModel(BaseModel):
+class VehiculosModel(BaseModel):    
     id:  PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    Modelo: int = Field(...)
+    Modelo: str = Field(...)
     Marca: str = Field(...)
     Año: int = Field(...)
     Tipo: str = Field(...)
-    Precio: int = Field(..., le=10)
+    Precio: int = Field(..., le=100000000)
 
     class Config:
         allow_population_by_field_name = True
@@ -48,12 +46,12 @@ class VehiculosModel(BaseModel):
                 "Marca" : "Mitsubishi",
                 "Año": 1998,
                 "Tipo": "Montero",
-                "Precio": 22000000
- }
- }
+                "Precio": 22000
+            }
+        }
 
 class UpdateVehiculosModel(BaseModel):
-    Modelo: Optional[int]
+    Modelo: Optional[str]
     Marca: Optional[str]
     Año: Optional[int]
     Tipo: Optional[str]
@@ -68,49 +66,44 @@ class UpdateVehiculosModel(BaseModel):
                 "Marca" : "Mitsubishi",
                 "Año": 1998,
                 "Tipo": "Montero",
-                "Precio": 22000000
-    }
- }
-@app.post("/", response_description="Add new vehiculo", response_model=VehiculosModel)
-async def create_vehiculo(vehiculo: VehiculosModel = Body(...)):
-    vehiculo = jsonable_encoder(vehiculo)
-    new_vehiculo = await db.insert_one(vehiculo)
-    created_vehiculo = await db.find_one({"_id":new_vehiculo.inserted_id})
+                "Precio": 22000
+                }
+            }
 
+@app.get("/")
+async def root():
+    return 'HOLA ESTA ES UNA PRUEBA'
+
+@app.post("/crear", response_description="Add new Vehiculos", response_model=VehiculosModel)
+async def create_Vehiculo(vehiculo: VehiculosModel = Body(...)):
+    student = jsonable_encoder(vehiculo)
+    new_vehiculo = await db["pruebavehiculos"].insert_one(vehiculo)
+    created_vehiculo = await db["pruebavehiculos"].find_one({"_id": new_vehiculo.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_vehiculo)
 
-
-@app.get("/", response_description="List all vehiculo",response_model=List[VehiculosModel])
-async def list_vehiculo():
-    vehiculo = await db.find().to_list(1000)
+@app.get("/list", response_description="List all Vehiculos", response_model=List[VehiculosModel])
+async def list_Vehiculos():
+    vehiculo = await db["pruebavehiculos"].find().to_list(1000)
     return vehiculo
-
 
 @app.get("/{id}", response_description="Get a single vehiculo",response_model=VehiculosModel)
 async def show_vehiculo(id: str):
-    if (vehiculo := await db.find_one({"_id": id})) is not None: return vehiculo
-
+    if (vehiculo := await db["pruebavehiculos"].find_one({"_id": id})) is not None:
+        return vehiculo
     raise HTTPException(status_code=404, detail=f"vehiculo {id} not found")
-
 
 @app.put("/{id}", response_description="Update a vehiculo",response_model=VehiculosModel)
-async def update_vehiculo(id: str, vehiculot: UpdateVehiculosModel = Body(...)):
-    vehiculo = {k: v for k, v in vehiculo.dict().items() if v is not None}
+async def update_vehiculo(id: str, vehiculo: UpdateVehiculosModel = Body(...)):
+    student = {k: v for k, v in vehiculo.dict().items() if v is not None}
+    
     if len(vehiculo) >= 1:
-        update_result = await db.update_one({"_id": id}, {"$set":vehiculo})
+        update_result = await db["pruebavehiculos"].update_one({"_id": id}, {"$set":vehiculo})
+        
         if update_result.modified_count == 1:
-            if (updated_vehiculo := await db.find_one({"_id": id})) is not None:
-
+            if (updated_vehiculo := await db["pruebavehiculos"].find_one({"_id": id})) is not None:
                 return updated_vehiculo
-    if (existing_vehiculo := await db.find_one({"_id": id})) is not None:
+
+    if (existing_vehiculo := await db["pruebavehiculos"].find_one({"_id": id})) is not None:
         return existing_vehiculo
 
-    raise HTTPException(status_code=404, detail=f"vehiculo {id} not found")
-
-
-@app.delete("/{id}", response_description="Delete a vehiculo")
-async def delete_vehiculo(id: str):
-    delete_result = await db.delete_one({"_id": id})
-    if delete_result.deleted_count == 1:
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
     raise HTTPException(status_code=404, detail=f"vehiculo {id} not found")
